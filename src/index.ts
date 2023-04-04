f452994f4e4cedaa95e88bc843b63218d5c0dff9
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-04-04 23:12:41 +0200

    Reorganized the error handling

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,27 @@ const userId = '5';
 const teamId = 'NOB';
 
 const doSomething = async (): Promise<void> => {
+  let user;
   try {
-    const user = await userModule.fetchUser(userId);
-    const team = await teamModule.rateTeam(teamId);
-
-    console.log(`User is: ${user}`);
-    console.log(`Team is: ${team}`);
-  } catch (err) {
+    user = await userModule.fetchUser(userId);
+   } catch(err) {
     if (err instanceof errorsModule.NotExistsError) {
-      console.error('Reached NotExistsError. But what is missing?');
+      console.error('Reached NotExistsError. We know it must be a user error');
       // handle error
-
-      /** Above does not contains the information on what's the entity that is missing.
-       *  The way to sort this is by adding one more instance of NotExistsError and then check the possible branches
-       * 
-       *  if (err instanceof UserNotExistsError) { ... }
-       *  if (err instanceof TeamNotExistsError) { ... }
-       */
     }
 
     if (err instanceof userModule.InvalidUserIdError) {
       console.error('Reached InvalidUserIdError. But what is missing?');
+      // handle error
+    }
+  }
+
+  let team;
+  try {
+    team = await teamModule.rateTeam(teamId);
+  } catch (err) {
+    if (err instanceof errorsModule.NotExistsError) {
+      console.error('Reached NotExistsError. We know it must be a team error');
       // handle error
     }
 
@@ -35,11 +35,19 @@ const doSomething = async (): Promise<void> => {
       // handle error
     }
   }
+
+  // We are safe here
+  console.log(`User is: ${user}`);
+  console.log(`Team is: ${team}`);
 }
 
 /**
- * In my opinion, this approach loses valuable information when something goes wrong, 
- * unless we dedicate more of boilerplate code around instanciating the possible errors.
- * And then, even if we do, we need to import the all the possible errors that we care about and handle them.
- * It's also a disrupting flow to think about, as it's branching the logic of the program.
+ * By handling the exceptions in this way we can use the contextual information without the need
+ * for a specialized instanciation of every error that might be repeated, like NotExistsError.
+ * It's still a lot of boilerplate IMO, and we still need to import every all the errors we might care about
+ * to check if "err instanceof". In this example this is doable, but the more instances we care about
+ * the more we need to track what errors each function might throw.
+ * On top of that, you don't even know if a function throws or not!
+ * 
+ * Next example will introduce a nested propagation of errors.
  */
